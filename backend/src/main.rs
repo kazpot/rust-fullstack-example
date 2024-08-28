c4ca946797de7da4ee32dd9895e7ec3210875e1d
kazpot <knman7@hotmail.com>
2024-08-28 18:48:07 +0900

    Update .gitignore

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -46,7 +46,6 @@ async fn main() {
         .layer(cors);
 
     tracing::info!("listening on port {}", "0.0.0.0:3000");
-    println!("listening on port {}", "0.0.0.0:3000");
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
